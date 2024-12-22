848f294261a67635444bb778b287a78d335d6089
eyadaq <eyadaq@live.com>
2024-12-22 23:20:50 +0300

    added the get env pathes

--- srcs/main.c
+++ srcs/main.c
@@ -6,39 +6,36 @@
 /*   By: eaqrabaw <eaqrabaw@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:58:16 by eaqrabaw          #+#    #+#             */
-/*   Updated: 2024/12/20 01:18:49 by eaqrabaw         ###   ########.fr       */
+/*   Updated: 2024/12/22 22:42:25 by eaqrabaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-void         ft_child_operation(char *argv[], char *envp[], int *fd)
+void ft_child_operation(char *argv[], char *envp[], int *fd)
 {
   int infile;
-
-  infile = open(argv[1], O_RDONLY);
-  if (-1 == infile)
-    ft_error("Couldnt Read the Infile");
-  
-    /*
-      * The child process will execute cmd1.
-      * It will redirect its standard output (stdout) to the write end of the pipe using dup2.
-      * This means that the output of cmd1 will be written to the pipe.
-    */
+	 
+	infile =open(fd[0], O_RDONLY, 0555);
+  if (fd == -1)
+  {
+    ft_error("Reading the file");
+    return (fd);   
+  }
+  return (fd);
 }
 
 void           ft_parent_operation(char *argv[], char *envp[], int *fd)
 {
   int outfile;
-
-  outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC);
-  if (-1 == outfile)
-    ft_error("Couldnt Create or Write to the Outfile");
-    /*
-      * The parent process will execute cmd2.
-      * It will redirect its standard input (stdin) to the read end of the pipe using dup2.
-      * This means that the input of cmd2 will come from the pipe.
-    */
+	 
+	outfile =open(fd[0], O_RDONLY, 0555);
+  if (fd == -1)
+  {
+    ft_error("Writing or creating the file");
+    return (fd);   
+  }
+  return (fd);
 }
 
 int     main(int argc, char *argv[], char *envp[])
--- srcs/utilities.c
+++ srcs/utilities.c
@@ -6,15 +6,44 @@
 /*   By: eaqrabaw <eaqrabaw@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 19:00:48 by eaqrabaw          #+#    #+#             */
-/*   Updated: 2024/12/19 22:35:21 by eaqrabaw         ###   ########.fr       */
+/*   Updated: 2024/12/22 22:42:20 by eaqrabaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-void ft_error(const char *s)
+void    ft_error(const char *s)
 {
 	perror(s);
 	exit(EXIT_FAILURE);
 }
+
+void    ft_get_path(char *env[], int file)
+{
+    int     i;
+    char    **pathes;
+    char    *path_value;
+
+    i = 0;
+    while (env[i])
+    {
+        if (strncmp(env[i], "PATH=", 5) == 0)
+        {
+            path_value = env[i];
+            break;
+        }
+        i++;
+    }
+    if (!env[i])
+    {
+        fprintf(stderr, "Error: PATH variable not found in the environment.\n");
+        return;
+    }
+    pathes = ft_split(path_value, ':');
+    i = 0;
+    while (pathes[i])
+        free(pathes[i++]);
+    free(pathes);
+}
+
 
