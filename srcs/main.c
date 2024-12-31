c6dbe214b16d8fabef7085417fce69849d8c563b
eyadaq <eyadaq@live.com>
2025-01-01 00:38:28 +0300

    Refactor command execution logic to improve error handling and streamline pipe operations; consolidate child process functions

--- srcs/main.c
+++ srcs/main.c
@@ -6,77 +6,74 @@
 /*   By: eaqrabaw <eaqrabaw@student.42amman.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/12/09 17:58:16 by eaqrabaw          #+#    #+#             */
-/*   Updated: 2024/12/31 23:32:14 by eaqrabaw         ###   ########.fr       */
+/*   Updated: 2025/01/01 00:38:06 by eaqrabaw         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/pipex.h"
 
-void	ft_cmd1_operation(char *argv[], char *envp[])
+void	ft_perror(const char *msg, int err_code)
 {
-	int   outfile;
+    perror(msg);
+    exit(err_code);
+}
 
-	outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
-	if (outfile < 0)
+void	ft_cmd1_operation(char *argv[], char *envp[], int pipe_fd[])
+{
+    int infile;
+
+    infile = open(argv[1], O_RDONLY);
+    if (infile < 0)
+        ft_perror("Open Failed", 4);
+    dup2(infile, STDIN_FILENO);
+    dup2(pipe_fd[1], STDOUT_FILENO);
+    close(pipe_fd[0]);
+    close(infile);
+    execve(argv[2], &argv[2], envp);
+    ft_perror("Execve Failed", 5);
+}
+
+void	ft_cmd2_operation(char *argv[], char *envp[], int pipe_fd[])
+{
+    int outfile;
+
+    outfile = open(argv[4], O_WRONLY | O_CREAT | O_TRUNC, 0644);
+    if (outfile < 0)
         ft_perror("File Creation Failed", 3);
-    exit(0);
+    dup2(pipe_fd[0], STDIN_FILENO);
+    dup2(outfile, STDOUT_FILENO);
+    close(pipe_fd[1]);
+    close(outfile);
+    execve(argv[3], &argv[3], envp);
+    ft_perror("Execve Failed", 5);
 }
 
-void    ft_cmd2_operation(char *argv[], char *envp[])
+int	main(int argc, char *argv[], char *envp[])
 {
-	int		infile;
-
-	infile = open(argv[1], O_RDONLY);
-	if (infile < 0)
-	{
-    	ft_perror("Open Failed", 4);
-	}
-	return ;
-}
-/*
-The first child handles reading from the input (or file) and executing the first command.
-The second child takes the output of the first child (via the pipe) as input and writes its result to the designated output (or file).
-*/
-void    first_child(char *argv[], char *envp[])
-{
-    int     pidid;
-
-    pidid = fork();
-    if (pidid = -1)
-    {
-        ft_perror("Fork Failed!", 1);
-    }
-    if (pidid == 0)
-    {
-        
-    }
-}
-
-void    second_child(char *argv[], char *envp[])
-{
-    int     pidid;
-
-    pidid = fork();
-    if (pidid = -1)
-    {
-        ft_perror("Fork Failed!", 1);
-    }
-    if (pidid == 0)
-    {
-        
-    }
-}
-
-int     main(int argc, char *argv[], char *envp[])
-{
-    int     pipe_fd[2];
-    int     infile;
-    int     outfile;
+    int pipe_fd[2];
+    int     pid1;
+    int      pid2;
 
     if (argc != 5)
     {
         ft_perror("Invalid input the input format is: ./pipex <file1> <cmd1> <cmd2> <file2>\n", -1);
         return (EXIT_FAILURE);
     }
-    
+    if (pipe(pipe_fd) == -1)
+        ft_perror("Pipe Failed", 2);
+    pid1 = fork();
+    if (pid1 == -1)
+        ft_perror("Fork Failed", 1);
+    if (pid1 == 0)
+        ft_cmd1_operation(argv, envp, pipe_fd);
+    pid2 = fork();
+    if (pid2 == -1)
+        ft_perror("Fork Failed", 1);
+    if (pid2 == 0)
+        ft_cmd2_operation(argv, envp, pipe_fd);
+    close(pipe_fd[0]);
+    close(pipe_fd[1]);
+    waitpid(pid1, NULL, 0);
+    waitpid(pid2, NULL, 0);
+    return (EXIT_SUCCESS);
 }
